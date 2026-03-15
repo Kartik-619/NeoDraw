75dd85762a236c7d7280686791b2870721de571c
Kartik Sharma <poojan4604@gmail.com>
2026-03-15 12:14:41 +0530

    did the ws server and all login relatedblueprint and yupp

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,31 +1,53 @@
 import express from "express";
-import  jwt  from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import { CreateUserSchema, CreateRoomSchema, SignInSchema } from "@repo/common/types";
 
 
-const app=express();
+const app = express();
 app.use(express.json());
 
-app.post("/signup",(req,res)=>{
-//dbcall
-
-})
-
-app.post("/signIn",(req,res)=>{
-    const userId=1;
-    const token= jwt.sign({
-        userId
-    },JWT_SECRET);
-
-    res.json({token});
-    
-})
-
-app.post("/room",(req,res)=>{
+app.post("/signup", (req, res) => {
     //dbcall
-    
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.json({
+            message: "invalid inputs"
+        });
+    }
+    return res.json({
+        userdId: '123'
+    })
+
+})
+
+app.post("/signIn", (req, res) => {
+    const data = SignInSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.json({
+            message: "invalid inputs"
+        });
+    }
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({ token });
+
+})
+
+app.post("/room", (req, res) => {
+    //dbcall
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.json({
+            message: "invalid inputs"
+        });
+    }
     res.json({
-        roomId:123
+        roomId: 123
     })
 })
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,5 +1,6 @@
 import { WebSocketServer } from "ws";
-
+import jwt, { decode, JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from '@repo/backend-common/config'; 
 const wss=new WebSocketServer({port:8080});
 
 wss.on('connection',function connection(ws,req){
@@ -8,7 +9,16 @@ wss.on('connection',function connection(ws,req){
         return;
     }
     const queryParams=new URLSearchParams(url.split('?')[1]);
-    const token=queryParams.get('token');
+    const token=queryParams.get('token') ||'';
+    const decoded =jwt.verify(token,JWT_SECRET);
+    if (typeof decoded =="string"){
+        ws.close();
+        return;
+    }
+    if(!decoded || !decoded.userId){
+        ws.close();
+        return;
+    }
 
     ws.on('message',function message(data){
         ws.send('pong');
