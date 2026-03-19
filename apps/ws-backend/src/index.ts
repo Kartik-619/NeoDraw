443a648c4950b89a660c96b48dc626d37b359118
Kartik Sharma <poojan4604@gmail.com>
2026-03-19 08:23:55 +0530

    adding persistance to chat and implementing ws server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,87 +1,99 @@
-import { WebSocketServer,WebSocket } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import jwt, { decode, JwtPayload } from "jsonwebtoken";
-import { JWT_SECRET } from '@repo/backend-common/config'; 
-const wss=new WebSocketServer({port:8080});
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { prisma } from "@repo/db/client";
+
+const wss = new WebSocketServer({ port: 8080 });
 
 
-interface User{
-ws:WebSocket,
-rooms:string[],
-userId:string
+interface User {
+    ws: WebSocket,
+    rooms: string[],
+    userId: string
 }
 
-const users:User[]=[];
+const users: User[] = [];
 
-function CheckUser(token:string):string|null{
+function CheckUser(token: string): string | null {
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-    
+
         if (typeof decoded == "string") {
-          return null;
+            return null;
         }
-    
+
         if (!decoded || !decoded.userId) {
-          return null;
+            return null;
         }
-    
+
         return decoded.userId;
-      } catch(e) {
+    } catch (e) {
         return null;
-      }
-      return null;
+    }
+
 }
 
-wss.on('connection',function connection(ws,req){
-    const url=req.url;
-    if(!url){
+wss.on('connection', function connection(ws, req) {
+    const url = req.url;
+    if (!url) {
         return;
     }
-    const queryParams=new URLSearchParams(url.split('?')[1]);
-    const token=queryParams.get('token') ||'';
-    const userId=CheckUser(token);
-    if(userId==null){
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') || '';
+    const userId = CheckUser(token);
+    if (userId == null) {
         ws.close();
         return;
     }
 
     users.push({
         userId,
-        rooms:[],
+        rooms: [],
         ws
     })
 
-    ws.on('message',function message(data){
-        try{
-            const parsedData=JSON.parse(data.toString()); //types->'join-room',roomId:1
-            if(parsedData.type==="join_room"){
-                const user=users.find(x=> x.ws ===ws);
+    ws.on('message', async function message(data) {
+        try {
+            const parsedData = JSON.parse(data.toString()); //types->'join-room',roomId:1
+            if (parsedData.type === "join_room") {
+                const user = users.find(x => x.ws === ws);
                 user?.rooms.push(parsedData.roomId);
             }
-    
-            if(parsedData.type==='leave_room'){
-                const user=users.find(x=>x.ws===ws);
-                if(!user){
+
+            if (parsedData.type === 'leave_room') {
+                const user = users.find(x => x.ws === ws);
+                if (!user) {
                     return;
                 }
-                user.rooms=user?.rooms.filter(x=>x===parsedData.room);
+                user.rooms = user?.rooms.filter(x => x === parsedData.room);
             }
-            if(parsedData.type==='chat'){
-                const roomId=parsedData.roomId;
-                const  msg=parsedData.message;
+            if (parsedData.type === 'chat') {
+                const roomId = parsedData.roomId;
+                const msg = parsedData.message;
 
-                users.forEach(user=>{
-                    if(user.rooms.includes(roomId)){
+                //push the message in a queue then to a DB rather than pushing directly in db to prevent slow speed
+                await prisma.chat.create({
+                    data: {
+                        roomId,
+                        msg,
+                        userId
+                    }
+                })
+
+
+                users.forEach(user => {
+                    if (user.rooms.includes(roomId)) {
                         user.ws.send(JSON.stringify({
-                            type:'chat',
-                            message:msg,
+                            type: 'chat',
+                            message: msg,
                             roomId
                         }))
                     }
                 })
             }
-        }catch(e){
+        } catch (e) {
 
         }
-        
+
     });
 })
