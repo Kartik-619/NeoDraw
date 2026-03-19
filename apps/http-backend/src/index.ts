9999c25653fca4c45ecc4cb6cc841aad7981893d
Kartik Sharma <poojan4604@gmail.com>
2026-03-19 10:29:42 +0530

    some fixes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -83,4 +83,19 @@ app.post("/room",async (req, res) => {
     })
 })
 
+app.get("/chats/:roomId", async (req,res)=>{
+    const roomId=Number(req.params.roomId);
+    const message=await prisma.chat.findMany({
+        where:{
+            roomId:roomId
+        },
+        orderBy:{
+            id:"desc"
+        },
+        take:50
+    });
+    res.json({
+        message
+    })
+})
 app.listen(3008);
