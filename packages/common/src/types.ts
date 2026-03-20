11af66fd5c1e6d95d96316666b3b44a03149129e
Kartik Sharma <poojan4604@gmail.com>
2026-03-20 13:44:12 +0530

    fixes

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -7,7 +7,8 @@ export const CreateUserSchema=z.object({
 });
 
 export const SignInSchema=z.object({ 
-name:z.string().min(3).max(16)
+name:z.string().min(3).max(16),
+password: z.string().min(8)
 });
 
 export const CreateRoomSchema=z.object({
