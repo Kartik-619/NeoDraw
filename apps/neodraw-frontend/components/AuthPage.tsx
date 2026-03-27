a4ab05841171813157c5b187874f9683bbc4eb85
Kartik Sharma <poojan4604@gmail.com>
2026-03-27 19:53:47 +0530

    some fixes in /signin

--- apps/neodraw-frontend/components/AuthPage.tsx
+++ apps/neodraw-frontend/components/AuthPage.tsx
@@ -1,23 +1,57 @@
 "use client";
 
-export function AuthPage({isSignin}: {
-    isSignin: boolean
-}) {
-    return <div className="w-screen h-screen flex justify-center items-center">
-        <div className="p-6 m-2 bg-white rounded">
-            <div className="p-2">
-                <input type="text" placeholder="Email"></input>
-            </div>
-            <div className="p-2">
-                
-            </div>
+import { useState } from "react";
+import axios from "axios";
 
-            <div className="pt-2">
-                <button className="bg-red-200 rounded p-2" onClick={() => {
+export function AuthPage({ isSignin }: { isSignin: boolean }) {
 
-                }}>{isSignin ? "Sign in" : "Sign up"}</button>
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    async function handleAuth(){
+        const endpoint = isSignin ? "signIn" : "signup"
+
+        const res = await axios.post(`http://localhost:3008/${endpoint}`,{
+            email,
+            password
+        })
+
+        localStorage.setItem("token",res.data.token)
+        window.location.href="/"
+    }
+
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="p-6 m-2 bg-white rounded">
+
+                <div className="p-2">
+                    <input 
+                        type="text"
+                        placeholder="Email"
+                        className="border p-2"
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div className="p-2">
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        className="border p-2"
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                </div>
+
+                <div className="pt-2">
+                    <button 
+                        className="bg-red-200 rounded p-2"
+                        onClick={handleAuth}
+                    >
+                        {isSignin ? "Sign in" : "Sign up"}
+                    </button>
+                </div>
+
             </div>
         </div>
-    </div>
-
+    )
 }
